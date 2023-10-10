a41f8ed61dcb688ff8668b07c202f20073771f1c
swain <iamswain@hotmail.com>
2023-10-10 20:58:43 +0900

    remove unused panels

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-export const ADDON_ID = "storybook/jotai-addon";
-export const PANEL_ID = `${ADDON_ID}/panel`;
-export const TAB_ID = `${ADDON_ID}/tab`;
-
-export const EVENTS = {
-  ATOMS_CHANGED: `${ADDON_ID}/atom_changed`,
-  RENDERED: `${ADDON_ID}/rendered`,
-};
--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,25 @@
-import { addons, makeDecorator } from "@storybook/preview-api";
-import { Atom, Provider, WritableAtom, createStore, useAtomValue } from "jotai";
-import React, { useEffect } from "react";
-import { EVENTS } from "./constants";
-
-type AtomHash = {
-  [key: string]: Atom<unknown>;
-};
-
-const Wrapper = ({ atoms, children }: { atoms: AtomHash; children: any }) => {
-  const channel = addons.getChannel();
-
-  const useAtoms: AtomHash = {};
-  Object.entries(atoms).forEach(([key, value]: [string, Atom<unknown>]) => {
-    useAtoms[key] = useAtomValue(value) as any;
-  });
-  const atomValues = Object.values(atoms);
-
-  useEffect(() => {
-    channel.emit(EVENTS.ATOMS_CHANGED, useAtoms);
-  }, [atoms, useAtoms, atomValues]);
-
-  return children;
-};
-
+import { makeDecorator } from "@storybook/preview-api";
+import {
+  getDefaultStore
+} from "jotai";
+export const ADDON_ID = "storybook/jotai-addon";
 export const withJotai = makeDecorator({
   name: "withJotai",
   parameterName: "jotai",
   skipIfNoParametersOrOptions: false, // Needs to be false so values get cleared
   wrapper: (storyFn, context, { parameters }) => {
-    const channel = addons.getChannel();
-
     if (!parameters) {
-      channel.emit(EVENTS.RENDERED, { note: "withJotai decorator not used" });
+      console.error("no jotai parameters");
       return storyFn(context);
     }
 
-    const store = createStore();
+    const store = getDefaultStore();
 
-    const { atoms, values } = parameters;
+    Object.values(parameters).forEach(([atom, value]) => {
+      store.set(atom, value);
+    });
 
-    Object.entries(atoms).map(
-      ([key, atom]: [string, WritableAtom<unknown, [any], unknown>]) =>
-        store.set(atom, values[key])
-    );
-
-    channel.emit(EVENTS.RENDERED, values);
-
-    return (
-      <Provider store={store}>
-        <Wrapper atoms={atoms}>{storyFn(context)}</Wrapper>
-      </Provider>
-    );
+    return storyFn(context);
   },
 });
 export default withJotai;
